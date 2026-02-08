fc3547a505bef5b77c238d97f5c99d64a3640461
Prathamesh <prathamesh66523@gmail.com>
2026-02-09 02:32:42 +0530

    app.py Homepage update 2

--- app.py
+++ app.py
@@ -4,50 +4,11 @@ from csr import run as run_csr
 from crr_cpt import run as run_crr_cpt
 from crr_spt import run as run_crr_spt
 from crr_dmt import run as run_crr_dmt
-from crr_vs  import run as run_crr_vs
+from crr_vs import run as run_crr_vs
 from crr_clay import run as run_crr_clay
 
-if "page" not in st.session_state:
-    st.session_state.page = "Home"
 
-st.set_page_config(page_title="Liquefaction Potential Analyser", layout="wide")
-
-st.sidebar.title("Liquefaction Toolkit")
-
-page = st.sidebar.radio(
-    "Analysis type",
-    [
-        "Home",
-        "CSR",
-        "CRR – CPT",
-        "CRR – SPT",
-        "CRR – DMT",
-        "CRR – Vs",
-        "CRR – Clay / Plastic silt",
-    ],
-    index=0
-)
-
-st.session_state.page = page
-
-
-if st.session_state.page == "Home":
-    home()
-elif st.session_state.page == "CSR":
-    run_csr()
-elif st.session_state.page == "CRR – CPT":
-    run_crr_cpt()
-elif st.session_state.page == "CRR – SPT":
-    run_crr_spt()
-elif st.session_state.page == "CRR – DMT":
-    run_crr_dmt()
-elif st.session_state.page == "CRR – Vs":
-    run_crr_vs()
-elif st.session_state.page == "CRR – Clay / Plastic silt":
-    run_crr_clay()
-
-
-
+# ---------- HOME PAGE ----------
 def home():
     st.title("Liquefaction Potential Assessment")
     st.caption("IS 1893 (2025) aligned • Mobile friendly")
@@ -78,3 +39,50 @@ def home():
             st.session_state.page = "CRR – Clay / Plastic silt"
             st.rerun()
 
+
+# ---------- PAGE STATE ----------
+if "page" not in st.session_state:
+    st.session_state.page = "Home"
+
+
+st.set_page_config(
+    page_title="Liquefaction Potential Analyser",
+    layout="wide"
+)
+
+
+# ---------- SIDEBAR ----------
+st.sidebar.title("Liquefaction Toolkit")
+
+page = st.sidebar.radio(
+    "Analysis type",
+    [
+        "Home",
+        "CSR",
+        "CRR – CPT",
+        "CRR – SPT",
+        "CRR – DMT",
+        "CRR – Vs",
+        "CRR – Clay / Plastic silt",
+    ],
+    index=0
+)
+
+st.session_state.page = page
+
+
+# ---------- ROUTER ----------
+if st.session_state.page == "Home":
+    home()
+elif st.session_state.page == "CSR":
+    run_csr()
+elif st.session_state.page == "CRR – CPT":
+    run_crr_cpt()
+elif st.session_state.page == "CRR – SPT":
+    run_crr_spt()
+elif st.session_state.page == "CRR – DMT":
+    run_crr_dmt()
+elif st.session_state.page == "CRR – Vs":
+    run_crr_vs()
+elif st.session_state.page == "CRR – Clay / Plastic silt":
+    run_crr_clay()
